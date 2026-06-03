006892b2a414a0679639c1cdeb015c9c2fb11afb
konishchukii <konishchukII@idft.ru>
2026-06-03 10:14:06 +0200

    Add Spring Boot application

---
+++ src/main/java/com/mesh/test_task/TestTaskMeshApplication.java
@@ -0,0 +1,13 @@
+package com.mesh.test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskMeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskMeshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mesh/test_task/TestTaskMeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesh.test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskMeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
